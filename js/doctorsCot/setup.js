17d1d9b9361ff2b9485e9dce0f8f9e8503edf9c9
Modestas Rukšnaitis <modestas@ruksnaitis.com>
2020-08-10 22:44:07 +0100

    Sherman's options now hide when other lang selected

--- js/doctorsCot/setup.js
+++ js/doctorsCot/setup.js
@@ -118,42 +118,22 @@ export const letter = [];
     }
 });
 
-// generate IPA keyboard from letter tables
-export function initIpaKeys() {
-    const ipaConsons = document.getElementById('ipa-consons');
-    const ipaVowels = document.getElementById('ipa-vowels');
+// create HTML button elements laid out same as letter table
+export function genKeyboard(elem, table) {
+		for (let styl in table) {
+				const tableRow = table[styl].trim().split(/\s+/); // turn string to array, removing white space
+				const keyRow = document.createElement('div');
 
-    // move through table, adding button to IPA keyboard
-    function genKeyboard(elem, table) {
-        for (let styl in table) {
-            const tableRow = table[styl].trim().split(/\s+/); // turn string to array, removing white space
-            const keyRow = document.createElement('div');
+				tableRow.forEach(letter => {
+						const keyInput = document.createElement('input');
 
-            tableRow.forEach(letter => {
-                const keyInput = document.createElement('input');
+						keyInput.type = "button";
+						keyInput.value = letter;
+						keyInput.onclick = () =>
+								document.getElementById('text').value += letter;
 
-                keyInput.type = "button";
-                keyInput.value = letter;
-                keyInput.onclick = () =>
-                    document.getElementById('text').value += letter;
-
-                keyRow.appendChild(keyInput);
-            });
-            elem.appendChild(keyRow);
-        }
-    }
-
-    genKeyboard(ipaConsons, consonantTable);
-    genKeyboard(ipaVowels, vowelTable);
-
-    const ipaKeys = document.getElementById('ipa-keys');
-    const langSelect = document.getElementById('language');
-
-    // only show IPA keyboard when Doctor's Cot selected
-    langSelect.addEventListener('input', event => {
-        if (event.target.value == "cot")
-            ipaKeys.classList.remove('hidden');
-        else
-            ipaKeys.classList.add('hidden');
-    });
+						keyRow.appendChild(keyInput);
+				});
+				elem.appendChild(keyRow);
+		}
 }
--- js/generalTranslate.js
+++ js/generalTranslate.js
@@ -1,37 +1,66 @@
 import { doctorsCotTranslate } from './doctorsCot/main.js'
-import { initIpaKeys } from './doctorsCot/setup.js'
+import { genKeyboard, consonantTable, vowelTable } from './doctorsCot/setup.js'
 
-initIpaKeys();
-
-//Clear canvas and pass word to specific language
 let x; //draw coordinate x
 let y; //draw coordinate y
+//Clear canvas and pass word to specific language
 function translate() {
-  var canvas = document.getElementById('canvas');
-  if (canvas.getContext) {
-    var ctx = canvas.getContext('2d');
+	var canvas = document.getElementById('canvas');
+	if (canvas.getContext) {
+		var ctx = canvas.getContext('2d');
 
-    ctx.setTransform(1,0,0,1,0,0);
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-  }
-  let input = document.getElementById("text").value;
-  let lang = document.getElementById("language").value;
-  switch (lang) {
-    case "shermans":
-      shermansTranslate(input);
-      break;
-    case "cot":
-      doctorsCotTranslate(ctx, input);
-      break;
-    case "tardis":
-      tardisTranslate(ctx, input);
-      break;
-    default:
-      shermansTranslate(input);
-  }
+		ctx.setTransform(1, 0, 0, 1, 0, 0);
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+	}
+	let input = document.getElementById("text").value;
+	let lang = document.getElementById("language").value;
+	switch (lang) {
+		case "shermans":
+			shermansTranslate(input);
+			break;
+		case "cot":
+			doctorsCotTranslate(ctx, input);
+			break;
+		case "tardis":
+			tardisTranslate(ctx, input);
+			break;
+		default:
+			shermansTranslate(input);
+	}
 }
 
+// Initialise event handlers and language-specific form controls
+const langSelect = document.getElementById('language');
+const langControls = document.getElementById('lang-controls');
+
+const shermansOpts = document.getElementById('shermans-options');
+
+const ipaKeys = document.getElementById('ipa-keys');
+const ipaConsons = document.getElementById('ipa-consons');
+const ipaVowels = document.getElementById('ipa-vowels');
+
+genKeyboard(ipaConsons, consonantTable);
+genKeyboard(ipaVowels, vowelTable);
+
+
+// only show IPA keyboard when Doctor's Cot selected
+langSelect.addEventListener('input', event => {
+	// First hide all controls
+	[...langControls.getElementsByClassName('active')]
+		.forEach(elem => elem.classList.remove('active'));
+
+	// Then display selected ones
+	switch (event.target.value) {
+		case "shermans":
+			shermansOpts.classList.add('active');
+			break;
+		case "cot":
+			ipaKeys.classList.add('active');
+			break;
+	}
+});
+
 document.forms[0].onsubmit = (event) => {
-  translate();
-  event.preventDefault();
+	translate();
+	event.preventDefault();
 };
