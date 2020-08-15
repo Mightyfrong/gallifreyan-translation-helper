a78b85572e70db95dcbf723a450274f6df6a655e
error on line 1 <dev@erroronline.one>
2020-08-15 18:04:29 +0200

    deletion of unneeded draw-instructions; beautifying file

--- js/shermans.js
+++ js/shermans.js
@@ -8,13 +8,13 @@ function shermansBase(char) {
 	let scgtable = {
 		"punctuation": [".", "?", "!", "\"", "'", "-", ",", ";", ":"],
 		"v": ["a", "e", "i", "o", "u"],
-		"b":["b", "ch", "d", "g", "h", "f"],
+		"b": ["b", "ch", "d", "g", "h", "f"],
 		"j": ["j", "ph", "k", "l", "c", "n", "p", "m"],
 		"t": ["t", "wh", "sh", "r", "v", "w", "s"],
 		"th": ["th", "gh", "y", "z", "q", "qu", "x", "ng"]
 	};
-	let rtrn="";
-	Object.keys(scgtable).forEach(function (row){
+	let rtrn = "";
+	Object.keys(scgtable).forEach(function (row) {
 		if (scgtable[row].indexOf(char) > -1) rtrn = row;
 	});
 	return rtrn;
@@ -24,16 +24,16 @@ function shermansBase(char) {
 function shermansDeco(char) {
 	let scgtable = {
 		"null": [".", "?", "!", "\"", "'", "-", ",", ",", ":", "a", "e", "i", "o", "u", "b", "j", "t", "th"],
-		"1l":["g", "n", "v", "qu"],
-		"2l":["h", "p", "w", "x"],
-		"3l":["f", "m", "s", "ng"],
-		"1d":["ph", "wh", "gh"],
-		"2d":["ch", "k", "sh", "y"],
-		"3d":["d", "l", "r", "z"],
-		"4d":["c", "q"]
+		"1l": ["g", "n", "v", "qu"],
+		"2l": ["h", "p", "w", "x"],
+		"3l": ["f", "m", "s", "ng"],
+		"1d": ["ph", "wh", "gh"],
+		"2d": ["ch", "k", "sh", "y"],
+		"3d": ["d", "l", "r", "z"],
+		"4d": ["c", "q"]
 	};
-	let rtrn="";
-	Object.keys(scgtable).forEach(function (row){
+	let rtrn = "";
+	Object.keys(scgtable).forEach(function (row) {
 		if (scgtable[row].indexOf(char) > -1) rtrn = row;
 	});
 	return rtrn == "null" ? "" : rtrn;
@@ -56,8 +56,7 @@ export function shermansTranslate(ctx, input) {
 	if (window.innerWidth < (50 + input.length * 50) * shermansScale) {
 		width = Math.floor(window.innerWidth / (50 * shermansScale)) * 50 * shermansScale - 50 * shermansScale;
 		height = (Math.ceil(((50 + input.length * 50) * shermansScale) / window.innerWidth)) * 120 * shermansScale + 50 * shermansScale;
-	}
-	else {
+	} else {
 		width = (50 + input.length * 50) * shermansScale;
 		height = 170 * shermansScale;
 	}
@@ -96,8 +95,7 @@ function shermansC(word) {
 			if (word[i + 1] == "k") continue; //omit ck
 			else if (["e", "i", "y"].indexOf(word[i + 1]) > -1) cword += "s";
 			else cword += "k"; //end of the word
-		}
-		else cword += word[i];
+		} else cword += word[i];
 	}
 	return cword;
 }
@@ -114,16 +112,17 @@ var shermansGrouped = {
 			var group = [];
 			if (document.getElementById('scgc').checked) sword = shermansC(sword);
 			for (var i = 0; i < sword.length; i++) { //iterate through word 
-				var current = sword[i], nexttwo = sword[i] + sword[i + 1];
+				var current = sword[i],
+					nexttwo = sword[i] + sword[i + 1];
 				//add double latin characters to group
 				if (["th", "gh", "ng", "qu", "wh", "sh", "ph", "ch"].indexOf(nexttwo) > -1) {
-					current=nexttwo;
+					current = nexttwo;
 					i++;
 				}
 				//add vowels if none or the same or consonants of same base to former group
 				if (document.getElementById('scgg').checked && group.length > 0 && (
-						(/*vowels */ shermansBase(current) == "v" && ("aeiou".indexOf(group[group.length - 1][group[group.length - 1].length - 1]) < 0 || current == group[group.length - 1][group[group.length - 1].length - 1]))
-						|| (/*same base consonant*/ shermansBase(current) != "punctuation" && shermansBase(current) != "v" && group[group.length - 1].length > 0 && shermansBase(current) == shermansBase(group[group.length - 1][group[group.length - 1].length - 1]))
+						( /*vowels */ shermansBase(current) == "v" && ("aeiou".indexOf(group[group.length - 1][group[group.length - 1].length - 1]) < 0 || current == group[group.length - 1][group[group.length - 1].length - 1])) ||
+						( /*same base consonant*/ shermansBase(current) != "punctuation" && shermansBase(current) != "v" && group[group.length - 1].length > 0 && shermansBase(current) == shermansBase(group[group.length - 1][group[group.length - 1].length - 1]))
 					))
 					group[group.length - 1].push(current)
 				else
@@ -135,7 +134,10 @@ var shermansGrouped = {
 	},
 	resetOffset: function () {
 		this.carriagereturn = false;
-		this.voweloffset = {"x": 0, "y": 0};
+		this.voweloffset = {
+			"x": 0,
+			"y": 0
+		};
 		this.vresize = 1;
 		this.cresize = 1;
 		this.offset = 0;
@@ -145,90 +147,88 @@ var shermansGrouped = {
 		this.offset++;
 		this.carriagereturn = true;
 		if (shermansBase(former) == "b") {
-			if (actual == "a") { }
-			else if (actual == "o") this.voweloffset = baseRelatedPosition("b");
-			else if (shermansBase(actual) == "b"){
+			if (actual == "a") {} else if (actual == "o") this.voweloffset = baseRelatedPosition("b");
+			else if (shermansBase(actual) == "b") {
 				this.cresize *= 1.2;
-				if (former != actual) this.linewidth +=1;
+				if (former != actual) this.linewidth += 1;
+			} else /*eiu*/ {
+				this.voweloffset.y = -22;
 			}
-			else /*eiu*/ { this.voweloffset.y = -22; }
-		}
-		else if (shermansBase(former) == "j") {
-			if (actual == "a") { }
-			else if (actual == "o") this.voweloffset = baseRelatedPosition("j");
-			else if (shermansBase(actual) == "j"){
+		} else if (shermansBase(former) == "j") {
+			if (actual == "a") {} else if (actual == "o") this.voweloffset = baseRelatedPosition("j");
+			else if (shermansBase(actual) == "j") {
 				this.cresize *= 1.2;
-				if (former != actual) this.linewidth +=1;
+				if (former != actual) this.linewidth += 1;
+			} else /*eiu*/ {
+				this.voweloffset.y = -25;
 			}
-			else /*eiu*/ { this.voweloffset.y = -25; }
-		}
-		else if (shermansBase(former) == "t") {
-			if (actual == "a") { }
-			else if (actual == "o") this.voweloffset = baseRelatedPosition("t");
-			else if (shermansBase(actual) == "t"){
+		} else if (shermansBase(former) == "t") {
+			if (actual == "a") {} else if (actual == "o") this.voweloffset = baseRelatedPosition("t");
+			else if (shermansBase(actual) == "t") {
 				this.cresize *= 1.2;
-				if (former != actual) this.linewidth +=1;
-			}
-			else /*eiu*/ { }
-		}
-		else if (shermansBase(former) == "th") {
-			if (actual == "a") { }
-			else if (actual == "o") this.voweloffset = baseRelatedPosition("th");
-			else if (shermansBase(actual) == "th"){
+				if (former != actual) this.linewidth += 1;
+			} else /*eiu*/ {}
+		} else if (shermansBase(former) == "th") {
+			if (actual == "a") {} else if (actual == "o") this.voweloffset = baseRelatedPosition("th");
+			else if (shermansBase(actual) == "th") {
 				this.cresize *= 1.2;
-				if (former != actual) this.linewidth +=1;
-			}
-			else /*eiu*/ { }
-		}
-		else /*vovel*/ {
+				if (former != actual) this.linewidth += 1;
+			} else /*eiu*/ {}
+		} else /*vovel*/ {
 			this.vresize *= 1.2;
-			if (actual == "a") { }
-			else if (actual == "o") { }
-			else /*eiu*/ { }
+			if (actual == "a") {} else if (actual == "o") {} else /*eiu*/ {}
 		}
 	}
 }
 
 //get relative x,y positions for line decorators depending on base
-function baseRelatedPosition(base, radiant){
+function baseRelatedPosition(base, radiant) {
 	if (radiant === undefined) radiant = Math.PI * .25;
 	switch (base) {
 		case "b":
-			return {"x": 20 * shermansGrouped.cresize * Math.cos(radiant), "y": 20 * shermansGrouped.cresize * Math.sin(radiant)};
+			return {
+				"x": 20 * shermansGrouped.cresize * Math.cos(radiant), "y": 20 * shermansGrouped.cresize * Math.sin(radiant)
+			};
 		case "j":
-			return {"x": 20 * shermansGrouped.cresize * Math.cos(radiant), "y": 20 * shermansGrouped.cresize * Math.sin(radiant)};
+			return {
+				"x": 20 * shermansGrouped.cresize * Math.cos(radiant), "y": 20 * shermansGrouped.cresize * Math.sin(radiant)
+			};
 		case "t":
-			return {"x": 20 * shermansGrouped.cresize * Math.cos(radiant), "y": 20 * shermansGrouped.cresize * Math.sin(radiant) - 25};
+			return {
+				"x": 20 * shermansGrouped.cresize * Math.cos(radiant), "y": 20 * shermansGrouped.cresize * Math.sin(radiant) - 25
+			};
 		case "th":
-			return {"x": 20 * shermansGrouped.cresize * Math.cos(radiant), "y": 20 * shermansGrouped.cresize * Math.sin(radiant) - 25};
+			return {
+				"x": 20 * shermansGrouped.cresize * Math.cos(radiant), "y": 20 * shermansGrouped.cresize * Math.sin(radiant) - 25
+			};
 	}
 }
 
-var draw={
-	init: function(ctx, linewidth){
+var draw = {
+	init: function (ctx, linewidth) {
 		this.ctx = ctx;
 		this.linewidth = linewidth;
 	},
-	circle:function(x, y, r, lw){
+	circle: function (x, y, r, lw) {
 		this.ctx.beginPath();
 		this.ctx.arc(x, y, r, 0, 2 * Math.PI, true);
 		if (lw !== undefined) this.ctx.lineWidth = lw;
 		this.ctx.stroke();
 		this.ctx.lineWidth = this.linewidth;
 	},
-	arc:function(x, y, r, a, o, lw){ //could be unified with circle with optional params but separated for readibilities sake...
+	arc: function (x, y, r, a, o, lw) { //could be unified with circle with optional params but separated for readibilities sake...
 		this.ctx.beginPath();
 		this.ctx.arc(x, y, r, a, o, true);
 		if (lw !== undefined) this.ctx.lineWidth = lw;
 		this.ctx.stroke();
 		this.ctx.lineWidth = this.linewidth;
 	},
-	dot:function(x, y, r, lw){
+	dot: function (x, y, r, lw) {
 		this.ctx.beginPath();
 		this.ctx.arc(x, y, r, 0, 2 * Math.PI, true);
 		this.ctx.fill();
 	},
-	line:function(fx, fy, tx, ty, lw){
+	line: function (fx, fy, tx, ty, lw) {
 		this.ctx.beginPath();
 		this.ctx.moveTo(fx, fy);
 		this.ctx.lineTo(tx, ty);
@@ -238,15 +238,13 @@ var draw={
 	}
 };
 
-
 //draw instructions for base + decoration
 function shermansDraw(ctx, letter, grouped) {
 	if (!grouped.carriagereturn) {
 		if (x + 50 * shermansScale >= width) {
 			y += 120 * shermansScale;
 			x = 0;
-		}
-		else x += 50 * shermansScale;
+		} else x += 50 * shermansScale;
 	}
 	if (letter != " ") {
 		ctx.strokeStyle = "black";
@@ -254,18 +252,19 @@ function shermansDraw(ctx, letter, grouped) {
 		if (letter == "c" || letter == "q") {
 			ctx.strokeStyle = 'red';
 			ctx.fillStyle = 'red';
-			if (letter == "c") { cLetter = true; }
-			else { qLetter = true; }
+			if (letter == "c") {
+				cLetter = true;
+			} else {
+				qLetter = true;
+			}
 		}
-		ctx.beginPath();
-		ctx.moveTo(x, y);
 		switch (shermansBase(letter)) {
 			case "punctuation":
 				draw.line(x, y, x + 50 * shermansScale, y);
 				draw.line(x, y + 25 * shermansScale, x + 50 * shermansScale, y + 25 * shermansScale);
 				switch (letter) {
 					case ".":
-						draw.circle(x + 25 * shermansScale, y + 25 * shermansScale, 10 * shermansScale);	
+						draw.circle(x + 25 * shermansScale, y + 25 * shermansScale, 10 * shermansScale);
 						break;
 					case "?":
 						draw.dot(x + 17.5 * shermansScale, y + 15 * shermansScale, 5 * shermansScale);
