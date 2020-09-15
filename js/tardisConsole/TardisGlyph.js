b385cfbf4b701789d2413e5a111030a6c24d6d44
Modestas Rukšnaitis <modestas@ruksnaitis.com>
2020-09-15 13:42:19 +0100

    added aleph deco

--- js/tardisConsole/TardisGlyph.js
+++ js/tardisConsole/TardisGlyph.js
@@ -12,7 +12,7 @@ export class TardisGlyph {
 
 	draw(ctx) {
 		if (this.consonant) {
-			this.consonant.draw(ctx)
+			this.consonant.draw(ctx);
 			if (this.vowel)
 				this.vowel.draw(ctx, this.consonant.vowelData);
 		}
@@ -20,25 +20,54 @@ export class TardisGlyph {
 			// Draw Aleph
 			ctx.save();
 			ctx.rotate(-π / 4);
-			ctx.beginPath();
+			ctx.fillStyle = '#fff';
+			ctx.lineWidth = 2;
 
-			ctx.save();
+			ctx.beginPath();
 			ctx.moveTo(-50 * Math.SQRT2, 0);
+			ctx.lineTo(50, 0);
+			ctx.stroke();
+
 			ctx.translate(-50, 0);
-			ctx.lineTo(0, 0);
 			[1, 2, 3].forEach(i => {
 				const r = 7.5 * i;
 				ctx.translate(r, 0);
-				ctx.moveTo(r, 0);
-				ctx.arc(0, 0, r, 0, 2*π);
-				ctx.translate(r + 5, 0);
-				ctx.lineTo(0, 0);
-			});
-			ctx.restore();
-			ctx.lineTo(50 * Math.SQRT2, 0);
 
-			ctx.lineWidth = 2;
-			ctx.stroke();
+				ctx.beginPath();
+				ctx.arc(0, 0, r, 0, 2 * π);
+				ctx.fill();
+				ctx.stroke();
+
+				ctx.save();
+				switch (this.vowel.toString) {
+					case 'A':
+						ctx.beginPath();
+						ctx.moveTo(-r, 0);
+						ctx.lineTo(r + 50 * (Math.SQRT2 - 1), 0);
+						ctx.stroke();
+					case 'O':
+						ctx.fillStyle = '#000';
+						ctx.beginPath();
+						ctx.arc(0, 0, r / 2, 0, 2 * π);
+						ctx.fill();
+						break;
+					case 'U':
+						ctx.fillStyle = '#000';
+					case 'I':
+						ctx.translate(0, r / 2);
+					case 'E':
+						ctx.lineWidth = 1;
+						ctx.translate(0, r / 2);
+						ctx.beginPath();
+						ctx.arc(0, 0, 2.5, 0, 2 * π);
+						ctx.fill();
+						ctx.stroke();
+				}
+				ctx.restore();
+
+				ctx.translate(r + 5, 0);
+			});
+
 			ctx.restore();
 		}
 	}
