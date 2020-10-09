70945c5ac385061fdc28267418d39ad2b582b9b9
error on line 1 <dev@erroronline.one>
2020-10-09 23:20:53 +0200

    where did the warning about unprocessable characters and tab-indentation even go in the first place?

--- js/cc/render.js
+++ js/cc/render.js
@@ -1,16 +1,19 @@
 import {
-    includes
+	includes
 } from '../utils/funcs.js'
 import {
-    ccBase,
-    ccDeco
+	ccBase,
+	ccDeco
 } from './ccGlyphs.js'
 import {
-    consonant
+	consonant
 } from './setup.js';
 import {
 	SVGRenderingContext
 } from '../utils/SVGRenderingContext.js';
+import {
+	UILanguage
+} from '../utils/UILanguage.js'
 
 let width; // canvas width
 let height; // canvas height
@@ -19,119 +22,133 @@ let y; // current coordinate y
 let letterwidth; // you'll figure that one out for yourself
 let letterheight; // you'll figure that one out for yourself
 
+// add module-specific language chunks
+UILanguage.say.processError = {
+	en: "The following characters could not be processed: ",
+	de: "Die folgenden Zeichen konnten nicht verarbeitet werden: ",
+	lt: "Nepavyko apdoroti šių simbolių: "
+};
+
 const base = new ccBase();
 const deco = new ccDeco();
 
-export function render( input) {
-    // convert input-string to grouped array and determine number of groups
-    let groupedinput = ccGrouped.groups(input.toLowerCase()),
-        maxstack = 1,
-        lettergroups = 0;
+export function render(input) {
+	// convert input-string to grouped array and determine number of groups
+	let groupedinput = ccGrouped.groups(input.toLowerCase()),
+		maxstack = 1,
+		lettergroups = 0,
+		warning = "";
 
-    groupedinput.forEach(word => {
-        word.forEach(groups => {
-            groups.forEach(group => { // determine maximum expansion due to stacking and amount of groups
-                if (maxstack < 1 + .6 * group.length) maxstack = 1 + .6 * group.length;
-                lettergroups++;
-            });
-        });
-        lettergroups++;
-    })
-    // initialize widths, heights, default-values, draw-object
-    letterwidth = consonant * maxstack;
-    letterheight = letterwidth * 3;
-    // set canvas scale according to number of groups times letterwidth
-    width = Math.min(lettergroups + 2, Math.floor(window.innerWidth / letterwidth)) * letterwidth - letterwidth;
-    height = letterheight * Math.ceil(lettergroups / Math.floor(width / letterwidth));
+	groupedinput.forEach(word => {
+		word.forEach(groups => {
+			groups.forEach(group => { // determine maximum expansion due to stacking and amount of groups
+				if (maxstack < 1 + .6 * group.length) maxstack = 1 + .6 * group.length;
+				lettergroups++;
+			});
+		});
+		lettergroups++;
+	})
+	// initialize widths, heights, default-values, draw-object
+	letterwidth = consonant * maxstack;
+	letterheight = letterwidth * 3;
+	// set canvas scale according to number of groups times letterwidth
+	width = Math.min(lettergroups + 2, Math.floor(window.innerWidth / letterwidth)) * letterwidth - letterwidth;
+	height = letterheight * Math.ceil(lettergroups / Math.floor(width / letterwidth));
 	const ctx = new SVGRenderingContext(width, height);
 
-    x = letterwidth*.5;
-    y = letterheight * .6;
+	x = letterwidth * .5;
+	y = letterheight * .6;
 
-    groupedinput.forEach(words => { // loop through sentence
-        words.forEach(groups => { // loop through words
-            groups.forEach(group => { // loop through character-groups 
-                // prepare resizing for stacked characters
-                var stack = group.length;
-                // reset offsets but hand over possible resizing factor
-                ccGrouped.resetOffset(stack);
-                // iterate through characters within group
-                for (var l = 0; l < group.length; l++) {
-                    // adjust offset properties according to former character/base
-                    if (l > 0) ccGrouped.setOffset();
-                    // draw
-                    ccDraw(ctx, group[l], ccGrouped);
-                }
-            });
-        });
-        ccGrouped.resetOffset();
-        ccDraw(ctx, " ", ccGrouped);
-    });
-    let output = "";
-    document.getElementById("output").innerHTML = output;
+	groupedinput.forEach(words => { // loop through sentence
+		words.forEach(groups => { // loop through words
+			groups.forEach(group => { // loop through character-groups 
+				// prepare resizing for stacked characters
+				var stack = group.length;
+				// reset offsets but hand over possible resizing factor
+				ccGrouped.resetOffset(stack);
+				// iterate through characters within group
+				for (var l = 0; l < group.length; l++) {
+					// adjust offset properties according to former character/base
+					if (l > 0) ccGrouped.setOffset();
+					// draw
+					if (base.getBase(group[l])) ccDraw(ctx, group[l], ccGrouped);
+					else warning += ", " + group[l];
+				}
+			});
+		});
+		ccGrouped.resetOffset();
+		ccDraw(ctx, " ", ccGrouped);
+	});
+
+	// complain about undefined characters
+	if (warning) document.getElementById("output").innerHTML = UILanguage.write("processError") + warning.substr(2);
+	else document.getElementById("output").innerHTML = "";
 
 	return ctx;
 }
 
 // set rules for grouping
 let ccGrouped = {
-    groups: function (input) {
-        // creates a multidimensional array for
-        // sentence -> words -> groups -> single letters
-        let sentence = [];
-        let splitinput = input.trim().replace(/\s+/g, " ").split(" "); // trim and strip multiple whitespaces, split input to single words and iterate through these
-        splitinput.forEach(sword => {
-            sentence.push([]); // init new word
-            let group = [];
-            let maxstack = document.getElementById("cc-stack").options[document.getElementById("cc-stack").selectedIndex].value;
-            for (var i = 0; i < sword.length; i++) { // iterate through word 
-                var current = sword[i],
-                    currenttwo = sword[i] + sword[i + 1];
-                // add double latin characters to group
-                if (includes(["th", "ng"], currenttwo)) {
-                    current = currenttwo;
-                    i++;
-                }
-                if (group.length > 0 && group[group.length - 1].length < maxstack) {
-                    // add to former group if not full
-                    group[group.length - 1].push(current)
-                } else // create current group
-                    group.push([current]);
-            }
-            sentence[sentence.length - 1].push(group); // append group to last word
-        });
-        return sentence;
-    },
-    resetOffset: function (stack) {
-        this.carriagereturn = false; // true overrides setting the pointer-position to the next character
-        this.resize = 1 + .6 * stack; // consonant-resize-factor, something the power of null is one
-        this.offset = 0; // counter of stacked objects, used for positioning the translated letters on top of the drawings
-    },
-    setOffset: function () {
-        this.offset++;
-        this.carriagereturn = true;
-        this.resize -= .6;
-    }
+	groups: function (input) {
+		// creates a multidimensional array for
+		// sentence -> words -> groups -> single letters
+		let sentence = [];
+		let splitinput = input.trim().replace(/\s+/g, " ").split(" "); // trim and strip multiple whitespaces, split input to single words and iterate through these
+		splitinput.forEach(sword => {
+			sentence.push([]); // init new word
+			let group = [];
+			let maxstack = document.getElementById("cc-stack").options[document.getElementById("cc-stack").selectedIndex].value;
+			for (var i = 0; i < sword.length; i++) { // iterate through word 
+				var current = sword[i],
+					currenttwo = sword[i] + sword[i + 1];
+				// add double latin characters to group
+				if (includes(["th", "ng"], currenttwo)) {
+					current = currenttwo;
+					i++;
+				}
+				if (group.length > 0 && group[group.length - 1].length < maxstack) {
+					// add to former group if not full
+					group[group.length - 1].push(current)
+				} else // create current group
+					group.push([current]);
+			}
+			sentence[sentence.length - 1].push(group); // append group to last word
+		});
+		return sentence;
+	},
+	resetOffset: function (stack) {
+		this.carriagereturn = false; // true overrides setting the pointer-position to the next character
+		this.resize = 1 + .6 * stack; // consonant-resize-factor, something the power of null is one
+		this.offset = 0; // counter of stacked objects, used for positioning the translated letters on top of the drawings
+	},
+	setOffset: function () {
+		this.offset++;
+		this.carriagereturn = true;
+		this.resize -= .6;
+	}
 }
 
 // draw instructions for base + decoration
 function ccDraw(ctx, letter, grouped) {
-    if (!grouped.carriagereturn) { // if not grouped set pointer to next letter position or initiate next line if canvas boundary is reached
-        if (x + letterwidth*2 >= width) {
-            y += letterheight;
-            x = letterwidth*1.5;
-        } else x += letterwidth;
-    }
-    //define tilt based on stack-number to make the glyphs less monotonous
-    let tilt = .25 - .0625 * (grouped.offset + 1);
-    // draw base
-    if (base.getBase(letter)) base.cctable[base.getBase(letter)].draw(ctx,x, y, consonant * grouped.resize, tilt);
-    // draw decorators
-    if (deco.getDeco(letter)) deco.cctable[deco.getDeco(letter)].draw(ctx,x, y, consonant * grouped.resize, tilt);
+	if (!grouped.carriagereturn) { // if not grouped set pointer to next letter position or initiate next line if canvas boundary is reached
+		if (x + letterwidth * 2 >= width) {
+			y += letterheight;
+			x = letterwidth * 1.5;
+		} else x += letterwidth;
+	}
+	//define tilt based on stack-number to make the glyphs less monotonous
+	let tilt = .25 - .0625 * (grouped.offset + 1);
+	// draw base
+	if (base.getBase(letter)) base.cctable[base.getBase(letter)].draw(ctx, x, y, consonant * grouped.resize, tilt);
+	// draw decorators
+	if (deco.getDeco(letter)) deco.cctable[deco.getDeco(letter)].draw(ctx, x, y, consonant * grouped.resize, tilt);
 
-    // text output for undefined characters as well for informational purpose
-    // print character translation above the drawings
-    ctx.drawText(letter, {x:x + grouped.offset * 10, y:y - letterheight * .5});
+	// text output for undefined characters as well for informational purpose
+	// print character translation above the drawings
+	ctx.drawText(letter, {
+		x: x + grouped.offset * 10,
+		y: y - letterheight * .5
+	});
 }
 
 /**Copyright 2020 Mightyfrong, erroronline1, ModisR
