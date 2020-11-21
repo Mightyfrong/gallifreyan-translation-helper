37f721b7a7d7e25170c5bcc2bb827e3978b8de31
error on line 1 <dev@erroronline.one>
2020-11-21 17:12:19 +0100

    added circular display

--- js/darklfaerie/render.js
+++ js/darklfaerie/render.js
@@ -1,26 +1,22 @@
 import {
-	includes
-} from '../utils/funcs.js'
-import {
 	UILanguage
 } from '../utils/UILanguage.js'
 import {
 	SVGRenderingContext
 } from '../utils/SVGRenderingContext.js';
-
 import {
 	dfGlyphs
 } from './darklfaeriesGlyphs.js';
+import {
+	character,
+	lwfactor
+} from './setup.js';
 
-
-let consonant = 40; // radius of consonants
-let linewidth = 1; // thicker lines add a cute chubbyness
 let width; // canvas width
 let height; // canvas height
 let x; // current coordinate x
 let y; // current coordinate y
-let letterwidth; // you'll figure that one out for yourself
-let letterheight; // you'll figure that one out for yourself
+let glyph; // global scope for glyph dimensions
 let warning = ""; // used if undefined characters are part of the input
 
 // add module-specific language chunks
@@ -34,36 +30,85 @@ UILanguage.say.processError = {
 export function render(input) {
 	// initialize widths, heights, default-values, draw-object
 	warning = "";
-	input = input.toLowerCase();
-	letterwidth = consonant * 2.25;
-	letterheight = consonant * 3;
+	let displayCircular = document.getElementById('dlscircular').checked,
+		groupedInput = input.toLowerCase().split(/\s+/),
+		charX, charY, textX, textY, dia;
 
-	x = letterwidth*-.5;
-	y = letterheight * .6;
-
+	if (displayCircular) {
+		let longest = groupedInput.slice();
+		dia = wordcircleRadius(longest.sort(sizesort)[0].length + 1, character) * 2.25;
+		glyph = {
+			width: dia,
+			height: dia,
+			count: groupedInput.length
+		};
+		x = y = glyph.width * .5;
+	} else {
+		glyph = {
+			width: character * 2.25,
+			height: character * 3,
+			count: input.length
+		};
+		x = glyph.width * -.5;
+		y = glyph.height * .6;
+	}
 	// set canvas scale according to number of characters
-	width = Math.min(input.length, Math.floor(window.innerWidth / letterwidth)) * letterwidth;
-	height = letterheight * Math.ceil(input.length / Math.floor(window.innerWidth / letterwidth));
+	width = Math.min(glyph.count, Math.floor(window.innerWidth / glyph.width)) * glyph.width;
+	height = glyph.height * Math.ceil(glyph.count / Math.floor(window.innerWidth / glyph.width));
 	const ctx = new SVGRenderingContext(width, height);
 
+	//return ctx;
 	// iterate through input
-	for (let i = 0; i < input.length; i++) {
-		// position pointer
-		if (x + letterwidth >= width) {
-			y += letterheight;
-			x = letterwidth;
+	groupedInput.forEach(word => {
+		if (displayCircular) {
+			// draw word circle
+			dia = wordcircleRadius(word.length + 1, character);
+			ctx.drawShape('circle', lwfactor * 2, {
+				cx: x,
+				cy: y,
+				r: dia - character * .5 / ((word.length + 1) ** 1.15)
+			});
 		}
-		else x += letterwidth;
-
-		if (input[i] in dfGlyphs) {
-
+		for (let i = 0; i < word.length; i++) {
+			// define center for character
+			if (displayCircular) {
+				let rad = 1.5 + 2 / (word.length) * i;
+				if (rad > 2) rad -= 2;
+				charX = x + Math.cos(Math.PI * rad) * dia * (1 - character / (dia - character * 1.6));
+				charY = y + Math.sin(Math.PI * rad) * dia * (1 - character / (dia - character * 1.6));
+				textX = x + Math.cos(Math.PI * rad) * dia * (1.1 - .4 / ((word.length + 1) ** 1.15));
+				textY = y + Math.sin(Math.PI * rad) * dia * (1.1 - .4 / ((word.length + 1) ** 1.15));
+			} else {
+				if (x + glyph.width >= width) {
+					y += glyph.height;
+					x = glyph.width;
+				} else x += glyph.width;
+				charX = x;
+				charY = y;
+				textX = x;
+				textY = y - character * 1.5;
+			}
 			// draw character
-			ctx.drawShape('circle',linewidth*2,{cx:x,cy:y,r:consonant});
-			dfDraw(ctx,x,y,input[i], dfGlyphs[input[i]]);
-
-		} else warning += ", " + input[i];
-
-	}
+			if (word[i] in dfGlyphs) {
+				ctx.drawShape('circle', lwfactor * 2, {
+					cx: charX,
+					cy: charY,
+					r: character
+				});
+				dfDraw(ctx, charX, charY, dfGlyphs[word[i]]);
+				// display character
+				ctx.drawText(word[i], {
+					x: textX,
+					y: textY
+				});
+			} else warning += ", " + word[i];
+		}
+		// position pointer for word circles or consider space between linear written words
+		if (x + glyph.width >= width) {
+			y += glyph.height;
+			x = glyph.width * (displayCircular ? .5 : 1);
+		} else x += glyph.width;
+	});
 
 	// complain about undefined characters
 	if (warning) document.getElementById("output").innerHTML = UILanguage.write("processError") + warning.substr(2);
@@ -72,37 +117,39 @@ export function render(input) {
 	return ctx;
 }
 
-function dfDraw(ctx, x,y, character, glyph) {
-	// this method assumes the provided paths have relative coordinates
+function wordcircleRadius(num, inner) {
+	num = Math.floor(num ** 1.15);
+	return (Math.ceil(Math.sqrt(num * Math.pow(2 * inner, 2) / Math.PI)) + inner);
+}
 
+function sizesort(a, b) {
+	if (a.length === b.length) return 0;
+	else return (a.length < b.length) ? 1 : -1;
+}
+
+function dfDraw(ctx, x, y, glyph) {
+	// this method assumes the provided paths have relative coordinates
 	let path;
 	const add = { // put x and y into usable scope, set correction value
-		x: x - consonant,
-		y: y - consonant,
-		scale: consonant / 23.5 // manually determined and totally dependable of inkscape canvas
+		x: x - character,
+		y: y - character,
+		scale: character / 23.5 // manually determined and totally dependable of inkscape canvas
 	};
-	glyph.forEach(p=>{
-
-	path = p.path.replace(/-{0,1}\d+\.{0,1}\d*,-{0,1}\d+\.{0,1}\d*/g, coords => { // find all numbers and rescale
-		let c = coords.split(",");
-		return (parseFloat(c[0]) * add.scale) + "," + (parseFloat(c[1]) * add.scale);
+	glyph.forEach(p => { // loop through thick and slim paths
+		// find all coordinates and rescale
+		path = p.path.replace(/-{0,1}\d+\.{0,1}\d*,-{0,1}\d+\.{0,1}\d*/g, coords => {
+			let c = coords.split(",");
+			return (parseFloat(c[0]) * add.scale) + "," + (parseFloat(c[1]) * add.scale);
+		});
+		// alter first coordinate by current position
+		path = path.replace(/-{0,1}\d+\.{0,1}\d*,-{0,1}\d+\.{0,1}\d*/, coords => {
+			let c = coords.split(",");
+			return (parseFloat(c[0]) + add.x) + "," + (parseFloat(c[1]) + add.y);
+		});
+		ctx.drawShape('path', p.lineweight * lwfactor, {
+			d: path
+		});
 	});
-
-	path = path.replace(/-{0,1}\d+\.{0,1}\d*,-{0,1}\d+\.{0,1}\d*/, coords => { // find first coordinate to add current position
-		let c = coords.split(",");
-		return (parseFloat(c[0]) + add.x) + "," + (parseFloat(c[1]) + add.y);
-	});
-
-console.log (p.path,path);
-	ctx.drawShape('path', p.lineweight*linewidth, {
-		d: path
-	});
-});
-ctx.drawText(character, {
-	x: x,
-	y: y - consonant * 1.5
-});
-
 }
 
 /**Copyright 2020 Mightyfrong, erroronline1, ModisR
