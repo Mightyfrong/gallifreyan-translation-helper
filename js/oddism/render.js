c6876001b1ca6c7b5827d28e15e932a37bdb1e42
erroronline1 <dev@erroronline.one>
2020-11-28 18:05:17 +0100

    overhauling characters to latest oddism-sheet

--- js/oddism/render.js
+++ js/oddism/render.js
@@ -50,7 +50,7 @@ export function render(input) {
 		let current = input[i],
 			currenttwo = input[i] + input[i + 1];
 		// add double latin characters to group
-		if (includes(["aʊ", "eɪ", "əʊ", "ɔɪ", "ɛə", "ɪə", "aɪ", "ʊə", "ju","t͡ʃ", "d͡ʒ"], currenttwo)) {
+		if (includes(["aʊ", "eɪ", "əʊ", "ɔɪ", "ɛə", "ɪə", "aɪ", "ʊə", "oʊ", "ju","tʃ", "dʒ"], currenttwo)) {
 			current = currenttwo;
 			i++;
 		}
