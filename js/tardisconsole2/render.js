ee1dca78ebff9502c752bb23fd6f6b21bec2870f
erroronline1 <dev@erroronline.one>
2021-02-13 23:35:04 +0100

    proper character grouping and fetching glyph directions as consonant property or vowel function return

--- js/tardisconsole2/render.js
+++ js/tardisconsole2/render.js
@@ -11,6 +11,9 @@ import {
 	character,
 	lwfactor
 } from './setup.js';
+import {
+	includes
+} from '../utils/funcs.js';
 
 let width; // canvas width
 let height; // canvas height
@@ -31,7 +34,7 @@ export function render(input) {
 	// initialize widths, heights, default-values, draw-object
 	warning = "";
 	let displayCircular = document.getElementById('tccircular').checked,
-		groupedInput = input.toLowerCase().split(/\s+/),
+		groupedInput = tardisCharacterGrouping(input),
 		charX, charY, textX, textY, dia;
 
 	if (displayCircular) {
@@ -54,49 +57,52 @@ export function render(input) {
 	}
 	// set canvas scale according to number of characters
 	width = Math.min(glyph.count, Math.floor(window.innerWidth / glyph.width) || 1) * glyph.width;
-	height = glyph.height * Math.ceil(glyph.count / (Math.floor(window.innerWidth / glyph.width)||1));
+	height = glyph.height * Math.ceil(glyph.count / (Math.floor(window.innerWidth / glyph.width) || 1));
 	const ctx = new SVGRenderingContext(width, height);
 
 	//return ctx;
 	// iterate through input
 	groupedInput.forEach(word => {
-		if (displayCircular) {
-			// draw word circle
-			dia = wordcircleRadius(word.length + 1, character);
-			ctx.drawShape('circle', lwfactor * 2, {
-				cx: x,
-				cy: y,
-				r: dia - character * .5 / ((word.length + 1) ** 1.15)
-			});
-		}
-		for (let i = 0; i < word.length; i++) {
-			// define center for character
 			if (displayCircular) {
-				let rad = 1.5 + 2 / (word.length) * i;
-				if (rad > 2) rad -= 2;
-				charX = x + Math.cos(Math.PI * rad) * dia * (1 - character / (dia - character * 1.6));
-				charY = y + Math.sin(Math.PI * rad) * dia * (1 - character / (dia - character * 1.6));
-				textX = x + Math.cos(Math.PI * rad) * dia * (1.1 - .4 / ((word.length + 1) ** 1.15));
-				textY = y + Math.sin(Math.PI * rad) * dia * (1.1 - .4 / ((word.length + 1) ** 1.15));
-			} else {
-				if (x + glyph.width >= width) {
-					y += glyph.height;
-					x = glyph.width;
-				} else x += glyph.width;
-				charX = x;
-				charY = y;
-				textX = x;
-				textY = y - character * 1.5;
-			}
-			// draw character
-			if (word[i] in consoleGlyphs) {
-				tcDraw(ctx, charX, charY, consoleGlyphs[word[i]]);
-				// display character
-				ctx.drawText(word[i], {
-					x: textX,
-					y: textY
+				// draw word circle
+				dia = wordcircleRadius(word.length + 1, character);
+				ctx.drawShape('circle', lwfactor * 2, {
+					cx: x,
+					cy: y,
+					r: dia - character * .5 / ((word.length + 1) ** 1.15)
 				});
-			} else warning += ", " + word[i];
+			}
+			for (let i = 0; i < word.length; i++) {
+				// define center for character
+				if (displayCircular) {
+					let rad = 1.5 + 2 / (word.length) * i;
+					if (rad > 2) rad -= 2;
+					charX = x + Math.cos(Math.PI * rad) * dia * (1 - character / (dia - character * 1.6));
+					charY = y + Math.sin(Math.PI * rad) * dia * (1 - character / (dia - character * 1.6));
+					textX = x + Math.cos(Math.PI * rad) * dia * (1.1 - .4 / ((word.length + 1) ** 1.15));
+					textY = y + Math.sin(Math.PI * rad) * dia * (1.1 - .4 / ((word.length + 1) ** 1.15));
+				} else {
+					if (x + glyph.width >= width) {
+						y += glyph.height;
+						x = glyph.width;
+					} else x += glyph.width;
+					charX = x;
+					charY = y;
+					textX = x;
+					textY = y - character * 1.5;
+				}
+				// draw character chunks
+				for (let j = 0; j < word[i].length; j++) {
+					if (word[i][j] in consoleGlyphs) {
+						tcDraw(ctx, charX, charY, includes(['a', 'e', 'i', 'o', 'u'], word[i][j]) ? consoleGlyphs[word[i][j]](word[i][j - 1]) : consoleGlyphs[word[i][j]]);
+					console.log(includes(['a', 'e', 'i', 'o', 'u'], word[i][j]) ? consoleGlyphs[word[i][j]](word[i][j - 1]) : consoleGlyphs[word[i][j]]);
+					} else warning += ", " + word[i][j];
+				}
+			// display character
+			ctx.drawText(word[i].join(''), {
+				x: textX,
+				y: textY
+			});
 		}
 		// position pointer for word circles or consider space between linear written words
 		if (x + glyph.width >= width) {
@@ -105,11 +111,41 @@ export function render(input) {
 		} else x += glyph.width;
 	});
 
-	// complain about undefined characters
-	if (warning) document.getElementById("output").innerHTML = UILanguage.write("processError") + warning.substr(2);
-	else document.getElementById("output").innerHTML = "";
+// complain about undefined characters
+if (warning) document.getElementById("output").innerHTML = UILanguage.write("processError") + warning.substr(2);
+else document.getElementById("output").innerHTML = "";
 
-	return ctx;
+return ctx;
+}
+
+function tardisCharacterGrouping(input) {
+	input = input.toLowerCase().split(/\s+/);
+	let output = [],
+		i = 0;
+	input.forEach(word => {
+		word = word.trim();
+		if (word.length) {
+			output.push([]);
+			for (i = 0; i < word.length; i++) {
+				output[output.length - 1].push([]);
+				let chunk = [output.length - 1, output[output.length - 1].length - 1],
+					current = word[i],
+					currenttwo = word[i] + word[i + 1];
+				if (includes(['ch', 'ng', 'qu', 'sh', 'th', 'ph'], currenttwo)) {
+					current = currenttwo;
+					i++;
+				}
+				if (output[chunk[0]][chunk[1] - 1] !== undefined && output[chunk[0]][chunk[1] - 1].length && !includes(['a', 'e', 'i', 'o', 'u'], output[chunk[0]][chunk[1] - 1]) && includes(['a', 'e', 'i', 'o', 'u'], current)) {
+					output[chunk[0]][chunk[1] - 1].push(current);
+					output[chunk[0]].pop();
+				} else if (!output[chunk[0]][chunk[1]].length && includes(['a', 'e', 'i', 'o', 'u'], current)) {
+					output[chunk[0]][chunk[1]].push('א', current);
+				} else
+					output[chunk[0]][chunk[1]].push(current);
+			}
+		}
+	});
+	return output;
 }
 
 function wordcircleRadius(num, inner) {
