7b11c4f2721ceb58107e5f3ee428098a1f0d585a
erroronline1 <dev@erroronline.one>
2021-04-10 13:41:46 +0200

    wip gc gallifreyan

---
+++ js/gc/gcGlyphs.js
@@ -0,0 +1,387 @@
+import {
+	renderOptions
+} from '../event_callbacks.js';
+import {
+	includes
+} from '../utils/funcs.js';
+
+// bases define shapes beforehand instead of drawing directly in hope of reusing these one day
+// when i am able to clip the paths of vowels accordingly
+
+export class gcGlyphs {
+	constructor() {
+		this.deco = {
+			b: {
+				contains: ["b", "j", "p", "v"],
+				draw: function (ctx, x, y, r, tilt) {
+					ctx.drawShape('circle', 1, {
+						cx: x + Math.cos(Math.PI * (1.25 + tilt)) * r,
+						cy: y + Math.sin(Math.PI * (1.25 + tilt)) * r,
+						r: r * .4,
+						fill: renderOptions.get().backgroundcolor
+					});
+				}
+			},
+			d: {
+				contains: ["d", "k", "q", "w"],
+				draw: function (ctx, x, y, r, tilt) {
+					ctx.drawShape('circle', 2, {
+						cx: x + Math.cos(Math.PI * (1.25 + tilt)) * r,
+						cy: y + Math.sin(Math.PI * (1.25 + tilt)) * r,
+						r: r * .4,
+						fill: renderOptions.get().backgroundcolor
+					});
+					ctx.drawShape('circle', 1, {
+						cx: x + Math.cos(Math.PI * (1.25 + tilt)) * r,
+						cy: y + Math.sin(Math.PI * (1.25 + tilt)) * r,
+						r: r * .3,
+						fill: renderOptions.get().backgroundcolor
+					});
+				}
+			},
+			f: {
+				contains: ["f", "l", "r", "x"],
+				draw: function (ctx, x, y, r, tilt) {
+					ctx.drawShape('circle', 0, {
+						cx: x + Math.cos(Math.PI * (1.25 + tilt)) * r,
+						cy: y + Math.sin(Math.PI * (1.25 + tilt)) * r,
+						r: r * .3
+					});
+				}
+			},
+			g: {
+				contains: ["g", "m", "s", "y"],
+				draw: function (ctx, x, y, r, tilt) {
+					let dir = Math.random() * 2;
+					ctx.drawShape('line', 1, {
+						x1: x + Math.cos(Math.PI * (dir + tilt)) * r * 1.6,
+						y1: y + Math.sin(Math.PI * (dir + tilt)) * r * 1.6,
+						x2: x,
+						y2: y
+					});
+					ctx.drawShape('line', 1, {
+						x1: x + Math.cos(Math.PI * (dir + .75 + tilt)) * r * 1.6,
+						y1: y + Math.sin(Math.PI * (dir + .75 + tilt)) * r * 1.6,
+						x2: x,
+						y2: y
+					});
+				}
+			},
+			h: {
+				contains: ["h", "n", "t", "z"],
+				draw: function (ctx, x, y, r, tilt) {
+					return;
+				}
+			}
+		};
+		this.base = {
+			b: {
+				contains: ["b", "d", "f", "g", "h"],
+				shape: function (x, y, r, tilt) {
+					return [{
+						shape: 'circle',
+						attributes: {
+							cx: x,
+							cy: y,
+							r: r,
+							fill: renderOptions.get().backgroundcolor
+						},
+						linewidth: 1
+					}];
+				},
+				draw: function (ctx, x, y, r, tilt) {
+					this.shape(x, y, r, tilt).forEach(s => {
+						ctx.drawShape(s.shape, s.linewidth, s.attributes);
+					});
+				}
+			},
+			j: {
+				contains: ["j", "k", "l", "m", "n"],
+				shape: function (x, y, r, tilt) {
+					let points = "";
+					for (let i = 0; i < 2; i += 2 / 6) {
+						points +=
+							(x + Math.cos(Math.PI * (i + tilt)) * r) +
+							"," +
+							(y + Math.sin(Math.PI * (i + tilt)) * r) +
+							" ";
+					}
+					return [{
+						shape: 'polygon',
+						attributes: {
+							points: points,
+							fill: renderOptions.get().backgroundcolor
+						},
+						linewidth: 1
+					}];
+				},
+				draw: function (ctx, x, y, r, tilt) {
+					this.shape(x, y, r, tilt).forEach(s => {
+						ctx.drawShape(s.shape, s.linewidth, s.attributes);
+					});
+				}
+			},
+			p: {
+				contains: ["p", "q", "r", "s", "t"],
+				shape: function (x, y, r, tilt) {
+					return [{
+						shape: 'circle',
+						attributes: {
+							cx: x,
+							cy: y,
+							r: r,
+							fill: renderOptions.get().backgroundcolor
+						},
+						linewidth: 2
+					}, {
+						shape: 'circle',
+						attributes: {
+							cx: x,
+							cy: y,
+							r: r * .9
+						},
+						linewidth: 1
+					}];
+				},
+				draw: function (ctx, x, y, r, tilt) {
+					this.shape(x, y, r, tilt).forEach(s => {
+						ctx.drawShape(s.shape, s.linewidth, s.attributes);
+					});
+				}
+			},
+			v: {
+				contains: ["v", "w", "x", "y", "z"],
+				shape: function (x, y, r, tilt) {
+					let points = ["", ""];
+					for (let i = 0; i < 2; i += 2 / 6) {
+						points[0] +=
+							(x + Math.cos(Math.PI * (i + tilt)) * r) +
+							"," +
+							(y + Math.sin(Math.PI * (i + tilt)) * r) +
+							" ";
+						points[1] +=
+							(x + Math.cos(Math.PI * (i + tilt)) * r * .9) +
+							"," +
+							(y + Math.sin(Math.PI * (i + tilt)) * r * .9) +
+							" ";
+					}
+					return [{
+						shape: 'polygon',
+						attributes: {
+							points: points[0],
+							fill: renderOptions.get().backgroundcolor
+						},
+						linewidth: 2
+					}, {
+						shape: 'polygon',
+						attributes: {
+							points: points[1]
+						},
+						linewidth: 1
+					}];
+				},
+				draw: function (ctx, x, y, r, tilt) {
+					this.shape(x, y, r, tilt).forEach(s => {
+						ctx.drawShape(s.shape, s.linewidth, s.attributes);
+					});
+				}
+			},
+		};
+		this.vowel = {
+			a: {
+				draw: function (ctx, x, y, r, tilt) {
+					ctx.drawShape('circle', 1, {
+						cx: x + Math.cos(Math.PI * (.5 + tilt)) * r,
+						cy: y + Math.sin(Math.PI * (.5 + tilt)) * r,
+						r: r * .75,
+						fill: renderOptions.get().backgroundcolor
+					});
+				}
+			},
+			e: {
+				draw: function (ctx, x, y, r, tilt) {
+					x = x + Math.cos(Math.PI * (.5 + tilt)) * r;
+					y = y + Math.sin(Math.PI * (.5 + tilt)) * r;
+					let points = "";
+					for (let i = 0; i < 2; i += 2 / 6) {
+						points +=
+							(x + Math.cos(Math.PI * (i + tilt)) * r * .75) +
+							"," +
+							(y + Math.sin(Math.PI * (i + tilt)) * r * .75) +
+							" ";
+					}
+					ctx.drawShape('polygon', 1, {
+						points: points,
+						fill: renderOptions.get().backgroundcolor
+					});
+				}
+			},
+			i: {
+				draw: function (ctx, x, y, r, tilt) {
+					ctx.drawShape('circle', 0, {
+						cx: x + Math.cos(Math.PI * (.5 + tilt)) * r,
+						cy: y + Math.sin(Math.PI * (.5 + tilt)) * r,
+						r: r * .75,
+						fill: renderOptions.get().backgroundcolor
+					});
+				}
+			},
+			o: {
+				draw: function (ctx, x, y, r, tilt) {
+					ctx.drawShape('circle', 1, {
+						cx: x + Math.cos(Math.PI * (.5 + tilt)) * r,
+						cy: y + Math.sin(Math.PI * (.5 + tilt)) * r,
+						r: r * .75,
+						fill: renderOptions.get().backgroundcolor
+					});
+					ctx.drawShape('circle', 1, {
+						cx: x + Math.cos(Math.PI * (.5 + tilt)) * r,
+						cy: y + Math.sin(Math.PI * (.5 + tilt)) * r,
+						r: r * .65,
+						fill: renderOptions.get().backgroundcolor
+					});
+				}
+			},
+			u: {
+				draw: function (ctx, x, y, r, tilt) {
+					x = x + Math.cos(Math.PI * (.5 + tilt)) * r;
+					y = y + Math.sin(Math.PI * (.5 + tilt)) * r;
+					let points = ["", ""];
+					for (let i = 0; i < 2; i += 2 / 6) {
+						points[0] +=
+							(x + Math.cos(Math.PI * (i + tilt)) * r * .75) +
+							"," +
+							(y + Math.sin(Math.PI * (i + tilt)) * r * .75) +
+							" ";
+						points[1] +=
+							(x + Math.cos(Math.PI * (i + tilt)) * r * .65) +
+							"," +
+							(y + Math.sin(Math.PI * (i + tilt)) * r * .65) +
+							" ";
+					}
+					ctx.drawShape('polygon', 1, {
+						points: points[0],
+						fill: renderOptions.get().backgroundcolor
+					});
+					ctx.drawShape('polygon', 1, {
+						points: points[1],
+						fill: renderOptions.get().backgroundcolor
+					});
+				}
+			}
+		};
+		this.punctuation = {
+			".": {
+				draw: function (ctx, x, y, r, tilt) {
+					ctx.drawShape('circle', 1, {
+						cx: x,
+						cy: y,
+						r: r
+					});
+					ctx.drawShape('path', 1,{
+						d: ctx.circularArc(x, y, r * .9, 1.75*Math.PI, 1.25*Math.PI, 'major')
+					});
+					ctx.drawShape('path', 1,{
+						d: ctx.circularArc(x, y, r * .9, 1.75*Math.PI, 1.25*Math.PI, 'minor')
+					});
+				}
+			},
+			",": {
+				draw: function (ctx, x, y, r, tilt) {
+					ctx.drawShape('path', 1,{
+						d: ctx.circularArc(x, y, r, 1.75*Math.PI, 1.25*Math.PI, 'major')
+					});
+					ctx.drawShape('path', 1,{
+						d: ctx.circularArc(x, y, r, 1.75*Math.PI, 1.25*Math.PI, 'minor')
+					});
+				}
+			},
+			"!": {
+				draw: function (ctx, x, y, r, tilt) {
+					ctx.drawShape('circle', 1, {
+						cx: x,
+						cy: y,
+						r: r*.9
+					});
+					ctx.drawShape('path', 1,{
+						d: ctx.circularArc(x, y, r, .75*Math.PI, .25*Math.PI, 'major')
+					});
+					ctx.drawShape('path', 1,{
+						d: ctx.circularArc(x, y, r, .75*Math.PI, .25*Math.PI, 'minor')
+					});
+				}
+			},
+			"?": {
+				draw: function (ctx, x, y, r, tilt) {
+					ctx.drawShape('path', 1,{
+						d: ctx.circularArc(x, y, r, .75*Math.PI, .25*Math.PI, 'major')
+					});
+					ctx.drawShape('path', 1,{
+						d: ctx.circularArc(x, y, r, .75*Math.PI, .25*Math.PI, 'minor')
+					});
+					ctx.drawShape('path', 1,{
+						d: ctx.circularArc(x, y, r * .9, 1.75*Math.PI, 1.25*Math.PI, 'major')
+					});
+					ctx.drawShape('path', 1,{
+						d: ctx.circularArc(x, y, r * .9, 1.75*Math.PI, 1.25*Math.PI, 'minor')
+					});
+				}
+			},
+			"'": {
+				draw: function (ctx, x, y, r, tilt) {
+					ctx.drawShape('circle', 1, {
+						cx: x,
+						cy: y,
+						r: r
+					});
+					ctx.drawShape('circle', 1, {
+						cx: x,
+						cy: y,
+						r: r * .9
+					});
+				}
+			},
+			'"': {
+				draw: function (ctx, x, y, r, tilt) {
+					ctx.drawShape('circle', 1, {
+						cx: x,
+						cy: y,
+						r: r
+					});
+				}
+			},
+		};
+	}
+	getDeco(char) { // return array of names of decorators the given character is assigned to
+		let rtrn = [];
+		Object.keys(this.deco).forEach(row => {
+			if (includes(this.deco[row].contains, char)) rtrn.push(row);
+		});
+		return (!rtrn.length || (rtrn.length == 1 && rtrn[0] == "null")) ? false : rtrn;
+	}
+	getBase(char) { // return array of names of bases the given character is assigned to
+		let rtrn = [];
+		Object.keys(this.base).forEach(row => {
+			if (includes(this.base[row].contains, char)) rtrn.push(row);
+		});
+		return (!rtrn.length || (rtrn.length == 1 && rtrn[0] == "null")) ? false : rtrn;
+	}
+}
+/**Copyright 2020-2021 Mightyfrong, erroronline1, ModisR
+ *
+ * This file is part of the Gallifreyan Translation Helper,
+ * henceforth referred to as "the GTH".
+ *
+ * The GTH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The GTH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the GTH.  If not, see <https://www.gnu.org/licenses/>.
+ */
---
+++ js/gc/render.js
@@ -0,0 +1,307 @@
+import {
+	includes,
+	dimensionObj
+} from '../utils/funcs.js';
+import {
+	glyphSize
+} from './setup.js';
+import {
+	gcGlyphs
+} from './gcGlyphs.js';
+import {
+	SVGRenderingContext
+} from '../utils/SVGRenderingContext.js';
+import {
+	unsupportedCharacters,
+	renderOptions
+} from '../event_callbacks.js';
+
+let canvas = {}; // canvas properties
+let option; // user selected render options handler
+let glyphs = { // glyph dimensions object
+	num: 0,
+	width: 0,
+	height: 0
+};
+let dimension = new dimensionObj(); // utility to calculate word-circle- and canvas dimensions
+
+const glyph = new gcGlyphs();
+
+export function render(input) {
+	//retrieve options and make them compact
+	option = renderOptions.get();
+
+	// convert input-string to grouped array and determine number of groups
+	let groupedInput = gcGrouped.groups(input.toLowerCase());
+	glyphs.num = 0; // reset for new input
+
+	if (!option.circular) {
+		groupedInput.forEach(sentence => {
+			sentence.forEach(word => {
+				word.forEach(group => {
+					if (!option.stacking) {
+						glyphs.num += group.length;
+						glyphs.width = glyphSize * 2.5;
+					} else {
+						let currentWidth = glyphSize * 1.1 * (group.length); // -1 because punctuation is always part of the last group
+						glyphs.width = glyphs.width < currentWidth ? currentWidth : glyphs.width;
+					}
+				});
+				if (option.stacking) glyphs.num += word.length;
+				glyphs.num += 1; //space after every word
+
+			});
+		});
+		glyphs.height = glyphSize * 4;
+	}
+	/*			groupedInput.forEach(sentence => {
+					circularGroups = 0;
+					for (let i = 0; i < sentence[0].length; i++) {
+						if (option.circular) {
+							let twc2;
+							if (!includes(" .!?‽", sentence[0][i]))
+								twc2 = dimension.wordcircleRadius(++circularGroups, glyphSize * stackedGlyph) * 1.5;
+							if (biggestWordCircle < twc2) biggestWordCircle = twc2;
+
+						} else {
+							glyphs.num++;
+						}
+					}
+				});
+	*/
+	let biggestWordCircle;
+	// set canvas scale according to number of letters/groups
+	if (false && option.circular) {
+		glyphs.num = groupedInput.length;
+		glyphs.width = biggestWordCircle;
+		glyphs.height = biggestWordCircle;
+		canvas["currentX"] = glyphs.width * .5;
+		canvas["currentY"] = -glyphs.height * .5;
+	} else {
+		canvas["currentX"] = -glyphs.width * .5;
+		canvas["currentY"] = glyphs.height * .5;
+	}
+	canvas["width"] = dimension.canvas(glyphs, option.maxWidth).width;
+	canvas["height"] = dimension.canvas(glyphs, option.maxWidth).height;
+
+	const ctx = new SVGRenderingContext(canvas.width, canvas.height);
+	let text;
+
+	// iterate through input to set grouping instructions, handle exceptions and draw glyphs
+	groupedInput.forEach(sentence => { // loop through sentence
+		sentence.forEach(word => { // loop through words
+			if (option.circular) {
+				text = "";
+				word.forEach(gr => {
+					text += gr.join('');
+				});
+			}
+			word.forEach(group => { // loop through groups
+				if (option.stacking) gcGrouped.resetOffset(group.length, group.join(''));
+
+				// iterate through characters within group
+				for (let l = 0; l < group.length; l++) {
+					if (!option.stacking) gcGrouped.resetOffset(1, group[l]);
+
+					gcDraw(ctx, group[l], gcGrouped);
+					if (!glyph.getDeco(group[l]) && !glyph.getBase(group[l]) &&
+						!(group[l] in glyph.vowel) && !(group[l] in glyph.punctuation))
+						unsupportedCharacters.add(group[l]);
+
+					gcGrouped.setOffset();
+				}
+			});
+			if (!option.circular) canvas = dimension.carriageReturn(canvas, glyphs, .5);
+		});
+		// gcGrouped.resetOffset();
+	});
+
+	// complain about unsupported characters
+	unsupportedCharacters.get();
+
+	return ctx;
+}
+
+// set rules for grouping
+let gcGrouped = {
+	groups: function (input) {
+		// creates a multidimensional array for
+		// sentence -> words -> groups -> single letters
+		input = input.trim().replace(/\s+/g, ' '); // trim and strip multiple whitespaces
+		if (!includes(".,!?'\"", input.substring(input.length - 1))) input += "."; // add punctuation by default if not provided
+		let sentences = input.match(/.+?[\?!,\.'"]/g); // divide into sentences by supported punctuation
+
+		let result = [];
+		sentences.forEach(sentence => {
+			let words = sentence.trim().split(/\s/g), // determine words by whitespace
+				sntnc = [];
+			words.forEach(word => {
+				word = replacements(word);
+				let groups = [];
+				for (var i = 0; i < word.length; i++) { // iterate through word 
+					if ((groups.length > 0) &&
+						!(includes(".,!?'\"", word[i]) // not adding punctuation 
+							||
+							(includes("aeiou", groups[groups.length - 1]) && !includes("aeiou", word[i])) // not adding consonant after vowel
+						)) {
+						// add to former group if allowed
+						groups[groups.length - 1].push(word[i])
+					} else // create current group
+						groups.push([word[i]]);
+				}
+				sntnc.push(groups); // append group to last sentence
+			});
+			result.push(sntnc);
+		});
+		return result;
+	},
+	resetOffset: function (stack, currentGroupText = '') {
+		this.carriagereturn = false; // true overrides setting the pointer-position to the next character
+		this.resize = 1; // glyph-resize-factor, something the power of null is one
+		this.offset = 0; // counter of stacked objects, used for positioning the translated letters on top of the drawings
+		this.currentGroupText = currentGroupText;
+		this.numberOfGroups = stack || 1;
+	},
+	setOffset: function () {
+		this.carriagereturn = true;
+		this.resize *= .7;
+		this.offset++;
+	}
+}
+
+//script specific replacements
+function replacements(word) {
+	let cword = "";
+	for (let i = 0; i < word.length; i++) { // iterate through word 
+		if (word[i] == "c") {
+			if (includes(["e", "i", "y"], word[i + 1])) cword += "s";
+			else cword += "k"; // end of the word
+		} else if (word[i] == "ß") cword += "ss";
+		else cword += word[i];
+	}
+	return cword;
+}
+
+// draw instructions for base + decoration
+function gcDraw(ctx, letter, grouped) {
+	//	if ((!option.circular || !grouped.glyph) &&
+	//		(!grouped.carriagereturn || (!grouped.carriagereturn && option.circular && !grouped.glyph))) { // if not grouped set pointer to next letter position or initiate next line if canvas boundary is reached
+	// position pointer
+	if (!grouped.carriagereturn) {
+		canvas = dimension.carriageReturn(canvas, glyphs, .5);
+	}
+	// rotation of charactergroups in regards of circular display
+	let rad = 0,
+		wordCircleRadius = glyphs.height,
+		center = {
+			x: 0,
+			y: 0
+		};
+	if (option.circular) {
+		rad = 1 + 2 / grouped.numberOfGroups * grouped.glyph;
+		wordCircleRadius /= 4;
+		center = { // relative center of sentence
+			x: -1 * (wordCircleRadius * Math.sin(Math.PI * rad)),
+			y: wordCircleRadius * Math.cos(Math.PI * rad)
+		};
+	}
+	//define tilt based on stack-number to distinguish between stacked characters
+	let tilt = .25 - (grouped.offset) * .1; //.0625;
+
+	let decoration = glyph.getDeco(letter),
+		base = glyph.getBase(letter);
+
+	if (decoration) glyph.deco[decoration].draw(ctx,
+		canvas.currentX + center.x,
+		canvas.currentY + center.y,
+		glyphSize * grouped.resize,
+		tilt);
+	if (base) glyph.base[base].draw(ctx,
+		canvas.currentX + center.x,
+		canvas.currentY + center.y,
+		glyphSize * grouped.resize,
+		tilt);
+	if (letter in glyph.vowel) {
+		glyph.vowel[letter].draw(ctx,
+			canvas.currentX + center.x,
+			canvas.currentY + center.y,
+			glyphSize * grouped.resize,
+			tilt);
+	}
+	if (letter in glyph.punctuation) {
+		glyph.punctuation[letter].draw(ctx,
+			canvas.currentX + center.x,
+			canvas.currentY + center.y,
+			glyphSize,
+			tilt);
+	}
+
+	/*	
+		
+		// draw consonant
+		if (letter in glyph.glyphs)
+			cwConsonants.glyphs[letter].draw(ctx,
+				canvas.currentX + center.x,
+				canvas.currentY + center.y,
+				glyphSize * grouped.resize,
+				tilt);
+		// draw vowel, smaller and randomly slightly offcentric
+		else if (letter in cwVowels.glyphs) {
+			let rot = Math.PI * Math.random() * 2;
+			cwVowels.glyphs[letter].draw(ctx,
+				canvas.currentX + center.x + Math.cos(rot) * glyphSize * grouped.resize * .75,
+				canvas.currentY + center.y + Math.sin(rot) * glyphSize * grouped.resize * .75,
+				glyphSize * .7,
+				tilt);
+		} else if (letter in cwPunctuation.glyphs) {
+			if (includes(",;", letter)) {
+				let rot = Math.PI * Math.random() * 2;
+				cwPunctuation.glyphs[letter].draw(ctx,
+					canvas.currentX + center.x + Math.cos(rot) * glyphSize * grouped.resize * .75,
+					canvas.currentY + center.y + Math.sin(rot) * glyphSize * grouped.resize * .75,
+					glyphSize * .7,
+					tilt);
+			} else {
+				cwPunctuation.glyphs[letter].draw(ctx,
+					canvas.currentX,
+					canvas.currentY,
+					(option.circular ? wordCircleRadius * 1.7 : glyphSize * grouped.resize)
+				);
+				if (option.circular) cwPunctuation.glyphs["start"].draw(ctx,
+					canvas.currentX + center.x,
+					canvas.currentY + center.y,
+					glyphSize * .7,
+					tilt);
+			}
+		}
+		*/
+
+	// text output for undefined characters as well for informational purpose
+	// print character translation above the drawings
+	let distance;
+	if (option.circular) distance = wordCircleRadius * 1.9;
+	else distance = glyphSize * 1.6; // * stackedGlyph;
+	if (grouped.offset == 0) ctx.drawText(grouped.currentGroupText, {
+		x: canvas.currentX - distance * Math.sin(Math.PI * rad),
+		y: canvas.currentY + distance * Math.cos(Math.PI * rad) + option.fontsize * .25
+	});
+}
+
+/**Copyright 2020-2021 Mightyfrong, erroronline1, ModisR
+ *
+ * This file is part of the Gallifreyan Translation Helper,
+ * henceforth referred to as "the GTH".
+ *
+ * The GTH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The GTH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the GTH.  If not, see <https://www.gnu.org/licenses/>.
+ */
