6abea7e1e49de2be0e86072f5cd190fa208cfe99
erroronline1 <dev@erroronline.one>
2021-04-23 21:48:12 +0200

    easier registration of modules

--- js/sw.js
+++ js/sw.js
@@ -1,4 +1,91 @@
-const cacheName = "cache2"; // Change value to force update
+const cacheName = "20210423_2145"; // Change value to force update
+
+const trinities = [ // have a glyphs-, setup- and render.js-file
+	"bpjmarriott",
+	"cbettenbender",
+	"cc",
+	"clockwork",
+	"darkifaerie",
+	"doctorsCot",
+	"eva",
+	"flux",
+	"gc",
+	"oddism",
+	"shermans",
+	"tardisconsole2"
+];
+const simpletons = [ // only have a render.js-file
+	"artbyboredom",
+	"dotscript"
+];
+const utils = [ // utils script files for easier registration
+	"funcs",
+	"MySelect",
+	"SVGRenderingContext",
+	"UILanguage"
+];
+
+let catchemall = [ // more or less static
+	"./",
+	// favicons
+	"./assets/favicon/android-chrome-36x36.png", // Favicon, Android Chrome M39+ with 0.75 screen density
+	"./assets/favicon/android-chrome-48x48.png", // Favicon, Android Chrome M39+ with 1.0 screen density
+	"./assets/favicon/android-chrome-72x72.png", // Favicon, Android Chrome M39+ with 1.5 screen density
+	"./assets/favicon/android-chrome-96x96.png", // Favicon, Android Chrome M39+ with 2.0 screen density
+	"./assets/favicon/android-chrome-144x144.png", // Favicon, Android Chrome M39+ with 3.0 screen density
+	"./assets/favicon/android-chrome-192x192.png", // Favicon, Android Chrome M39+ with 4.0 screen density
+	"./assets/favicon/android-chrome-256x256.png", // Favicon, Android Chrome M47+ Splash screen with 1.5 screen density
+	"./assets/favicon/android-chrome-384x384.png", // Favicon, Android Chrome M47+ Splash screen with 3.0 screen density
+	"./assets/favicon/android-chrome-512x512.png", // Favicon, Android Chrome M47+ Splash screen with 4.0 screen density
+	"./assets/favicon/apple-touch-icon.png", // Favicon, Apple default
+	"./assets/favicon/apple-touch-icon-57x57.png", // Apple iPhone, Non-retina with iOS6 or prior
+	"./assets/favicon/apple-touch-icon-60x60.png", // Apple iPhone, Non-retina with iOS7
+	"./assets/favicon/apple-touch-icon-72x72.png", // Apple iPad, Non-retina with iOS6 or prior
+	"./assets/favicon/apple-touch-icon-76x76.png", // Apple iPad, Non-retina with iOS7
+	"./assets/favicon/apple-touch-icon-114x114.png", // Apple iPhone, Retina with iOS6 or prior
+	"./assets/favicon/apple-touch-icon-120x120.png", // Apple iPhone, Retina with iOS7
+	"./assets/favicon/apple-touch-icon-144x144.png", // Apple iPad, Retina with iOS6 or prior
+	"./assets/favicon/apple-touch-icon-152x152.png", // Apple iPad, Retina with iOS7
+	"./assets/favicon/apple-touch-icon-180x180.png", // Apple iPhone 6 Plus with iOS8
+	"./assets/favicon/browserconfig.xml", // IE11 icon configuration file
+	"./assets/favicon/favicon.ico", // Favicon, IE and fallback for other browsers
+	"./assets/favicon/favicon-16x16.png", // Favicon, default
+	"./assets/favicon/favicon-32x32.png", // Favicon, Safari on Mac OS
+	"./assets/favicon/manifest.json", // Manifest file
+	"./assets/favicon/maskable_icon.png", // Favicon, maskable https://web.dev/maskable-icon
+	"./assets/favicon/mstile-70x70.png", // Favicon, Windows 8 / IE11
+	"./assets/favicon/mstile-144x144.png", // Favicon, Windows 8 / IE10
+	"./assets/favicon/mstile-150x150.png", // Favicon, Windows 8 / IE11
+	"./assets/favicon/mstile-310x150.png", // Favicon, Windows 8 / IE11
+	"./assets/favicon/mstile-310x310.png", // Favicon, Windows 8 / IE11
+	"./assets/favicon/safari-pinned-tab.svg", // Favicon, Safari pinned tab
+
+	// images
+	"./assets/star_field.png",
+	"./assets/time_lord_glow.svg",
+	"./assets/time_lord_silhouette",
+
+	// html and css
+	"./index.html", // Main HTML file
+	"./css/main.css", // Main CSS file
+	"./css/my-select.css",
+	"./css/my-slider.css",
+
+	// js modules
+	"./js/main.js",
+	"./js/event_callbacks.js"
+];
+
+// add modules and utils
+trinities.forEach(dir => {
+	catchemall.push("./js/" + dir + "/glyphs.js", "./js/" + dir + "/setup.js", "./js/" + dir + "/render.js")
+});
+simpletons.forEach(dir => {
+	catchemall.push("./js/" + dir + "/render.js")
+});
+utils.forEach(file => {
+	catchemall.push("./js/utils/" + file + ".js")
+});
 
 self.addEventListener("install", event => {
 	// Kick out the old service worker
@@ -6,113 +93,7 @@ self.addEventListener("install", event => {
 
 	event.waitUntil(
 		caches.open(cacheName).then(cache => {
-			return cache.addAll([
-				"./",
-				// favicons
-				"./assets/favicon/android-chrome-36x36.png", // Favicon, Android Chrome M39+ with 0.75 screen density
-				"./assets/favicon/android-chrome-48x48.png", // Favicon, Android Chrome M39+ with 1.0 screen density
-				"./assets/favicon/android-chrome-72x72.png", // Favicon, Android Chrome M39+ with 1.5 screen density
-				"./assets/favicon/android-chrome-96x96.png", // Favicon, Android Chrome M39+ with 2.0 screen density
-				"./assets/favicon/android-chrome-144x144.png", // Favicon, Android Chrome M39+ with 3.0 screen density
-				"./assets/favicon/android-chrome-192x192.png", // Favicon, Android Chrome M39+ with 4.0 screen density
-				"./assets/favicon/android-chrome-256x256.png", // Favicon, Android Chrome M47+ Splash screen with 1.5 screen density
-				"./assets/favicon/android-chrome-384x384.png", // Favicon, Android Chrome M47+ Splash screen with 3.0 screen density
-				"./assets/favicon/android-chrome-512x512.png", // Favicon, Android Chrome M47+ Splash screen with 4.0 screen density
-				"./assets/favicon/apple-touch-icon.png", // Favicon, Apple default
-				"./assets/favicon/apple-touch-icon-57x57.png", // Apple iPhone, Non-retina with iOS6 or prior
-				"./assets/favicon/apple-touch-icon-60x60.png", // Apple iPhone, Non-retina with iOS7
-				"./assets/favicon/apple-touch-icon-72x72.png", // Apple iPad, Non-retina with iOS6 or prior
-				"./assets/favicon/apple-touch-icon-76x76.png", // Apple iPad, Non-retina with iOS7
-				"./assets/favicon/apple-touch-icon-114x114.png", // Apple iPhone, Retina with iOS6 or prior
-				"./assets/favicon/apple-touch-icon-120x120.png", // Apple iPhone, Retina with iOS7
-				"./assets/favicon/apple-touch-icon-144x144.png", // Apple iPad, Retina with iOS6 or prior
-				"./assets/favicon/apple-touch-icon-152x152.png", // Apple iPad, Retina with iOS7
-				"./assets/favicon/apple-touch-icon-180x180.png", // Apple iPhone 6 Plus with iOS8
-				"./assets/favicon/browserconfig.xml", // IE11 icon configuration file
-				"./assets/favicon/favicon.ico", // Favicon, IE and fallback for other browsers
-				"./assets/favicon/favicon-16x16.png", // Favicon, default
-				"./assets/favicon/favicon-32x32.png", // Favicon, Safari on Mac OS
-				"./assets/favicon/manifest.json", // Manifest file
-				"./assets/favicon/maskable_icon.png", // Favicon, maskable https://web.dev/maskable-icon
-				"./assets/favicon/mstile-70x70.png", // Favicon, Windows 8 / IE11
-				"./assets/favicon/mstile-144x144.png", // Favicon, Windows 8 / IE10
-				"./assets/favicon/mstile-150x150.png", // Favicon, Windows 8 / IE11
-				"./assets/favicon/mstile-310x150.png", // Favicon, Windows 8 / IE11
-				"./assets/favicon/mstile-310x310.png", // Favicon, Windows 8 / IE11
-				"./assets/favicon/safari-pinned-tab.svg", // Favicon, Safari pinned tab
-
-				// images
-				"./assets/star_field.png",
-				"./assets/time_lord_glow.svg",
-				"./assets/time_lord_silhouette",
-
-				// html and css
-				"./index.html", // Main HTML file
-				"./css/main.css", // Main CSS file
-				"./css/my-select.css",
-
-				// js modules
-				"./js/main.js",
-				"./js/event_callbacks.js",
-
-				"./js/artbyboredom/render.js",
-
-				"./js/bpjmarriott/glyphs.js",
-				"./js/bpjmarriott/render.js",
-				"./js/bpjmarriott/setup.js",
-
-				"./js/cbettenbender/glyphs.js",
-				"./js/cbettenbender/render.js",
-				"./js/cbettenbender/setup.js",
-
-				"./js/cc/glyphs.js",
-				"./js/cc/render.js",
-				"./js/cc/setup.js",
-
-				"./js/clockwork/glyphs.js",
-				"./js/clockwork/render.js",
-				"./js/clockwork/setup.js",
-
-				"./js/darkifaerie/glyphs.js",
-				"./js/darkifaerie/render.js",
-				"./js/darkifaerie/setup.js",
-
-				"./js/doctorsCot/glyphs.js",
-				"./js/doctorsCot/render.js",
-				"./js/doctorsCot/setup.js",
-
-				"./js/dotscript/render.js",
-
-				"./js/eva/glyphs.js",
-				"./js/eva/render.js",
-				"./js/eva/setup.js",
-
-				"./js/flux/glyphs.js",
-				"./js/flux/render.js",
-				"./js/flux/setup.js",
-
-				"./js/gc/glyphs.js",
-				"./js/gc/render.js",
-				"./js/gc/setup.js",
-
-				"./js/oddism/glyphs.js",
-				"./js/oddism/render.js",
-				"./js/oddism/setup.js",
-
-				"./js/shermans/glyphs.js",
-				"./js/shermans/render.js",
-				"./js/shermans/setup.js",
-
-				"./js/tardisconsole2/glyphs.js",
-				"./js/tardisconsole2/render.js",
-				"./js/tardisconsole2/setup.js",
-
-				"./js/utils/funcs.js",
-				"./js/utils/MySelect.js",
-				"./js/utils/SVGRenderingContext.js",
-				"./js/utils/UILanguage.js",
-
-			]);
+			return cache.addAll(catchemall);
 		})
 	);
 });
