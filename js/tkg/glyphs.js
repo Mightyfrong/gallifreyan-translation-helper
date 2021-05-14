c5fc4bcf364263f630c9e3143fa02dcc4c890289
erroronline1 <dev@erroronline.one>
2021-05-15 00:08:31 +0200

    added timekeepers gallifreyan based on cc

---
+++ js/tkg/glyphs.js
@@ -0,0 +1,317 @@
+import {
+	includes,renderOptions
+} from '../main.js';
+
+//specify base for every letter, assign base to latin characters and specify geometric properties
+export class tkgBase {
+	constructor() {
+		this.tkgtable = {
+			/* example: { // name of group
+				contains: [...], // array of characters for which the handling and properties apply
+				draw: function (ctx,x,y,r,tilt){ // position of items to be placed, radius and tilt of graphics
+					SVGRenderingContext-draw instructions...
+				}
+			}*/
+			b: {
+				contains: ["b", "c", "d", "f", "g"],
+				draw: function (ctx, x, y, r, tilt) {
+					ctx.drawShape('circle', 1, {
+						cx: x,
+						cy: y,
+						r: r,
+						fill: renderOptions.get().backgroundcolor
+					});
+				}
+			},
+			h: {
+				contains: ["h", "j", "k", "l", "m"],
+				draw: function (ctx, x, y, r, tilt) {
+					ctx.drawShape('circle', 1, {
+						cx: x,
+						cy: y,
+						r: r,
+						fill: renderOptions.get().backgroundcolor
+					});
+					ctx.drawShape('circle', 1, {
+						cx: x,
+						cy: y,
+						r: r-3
+					});
+				}
+			},
+			n: {
+				contains: ["n", "p", "q", "r", "s",],
+				draw: function (ctx, x, y, r, tilt) {
+					ctx.drawShape('circle', 3, {
+						cx: x,
+						cy: y,
+						r: r,
+						fill: renderOptions.get().backgroundcolor
+					});
+					ctx.drawShape('circle', 1, {
+						cx: x,
+						cy: y,
+						r: r-4
+					});
+				}
+			},
+			t: {
+				contains: ["t", "v", "w", "x", "y"],
+				draw: function (ctx, x, y, r, tilt) {
+					ctx.drawShape('circle', 3, {
+						cx: x,
+						cy: y,
+						r: r,
+						fill: renderOptions.get().backgroundcolor
+					});
+				}
+			},
+			z: {
+				contains: ["z", "א", "th", "ng"],
+				draw: function (ctx, x, y, r, tilt) {
+					ctx.drawShape('circle', 1, {
+						cx: x,
+						cy: y,
+						r: r,
+						fill: renderOptions.get().backgroundcolor
+					});
+					ctx.drawShape('circle', 1, {
+						cx: x,
+						cy: y,
+						r: r-3
+					});
+					ctx.drawShape('circle', 1, {
+						cx: x,
+						cy: y,
+						r: r-6
+					});
+				}
+			}
+
+		}
+	}
+	getBase(char) { // return name of base the given character is assigned to
+		let rtrn = false;
+		Object.keys(this.tkgtable).forEach(row => {
+			if (includes(this.tkgtable[row].contains, char)) rtrn = row;
+		});
+		return rtrn;
+	}
+}
+
+// specify decoration for every letter
+export class tkgDeco {
+	constructor() {
+		this.tkgtable = {
+			/* example: { // name of group
+				contains: [...], // array of characters for which the handling and properties apply
+				draw: function (ctx, x,y,r,tilt){ // position of items to be placed, radius and tilt of graphics
+					SVGRenderingContext-draw instructions...
+				}
+			}*/
+			"circle": {
+				contains: ["b", "h", "n", "t","z"],
+				draw: function (ctx, x, y, r, element, tilt) {
+					ctx.drawShape('circle', 1, {
+						cx: x + Math.cos(Math.PI * (1.5 + tilt)) * (r-element*.6),
+						cy: y + Math.sin(Math.PI * (1.5 + tilt)) * (r-element*.6),
+						r: element * .6,
+						fill: renderOptions.get().backgroundcolor
+					});
+				}
+			},
+			"twocircles": {
+				contains: ["c", "j", "p", "v","א"],
+				draw: function (ctx, x, y, r, element, tilt) {
+					ctx.drawShape('circle', 1, {
+						cx: x + Math.cos(Math.PI * (1.6 + tilt)) * (r-element*.6),
+						cy: y + Math.sin(Math.PI * (1.6 + tilt)) * (r-element*.6),
+						r: element * .6,
+						fill: renderOptions.get().backgroundcolor
+					});
+					ctx.drawShape('circle', 1, {
+						cx: x + Math.cos(Math.PI * (.6 + tilt)) * (r-element*.6),
+						cy: y + Math.sin(Math.PI * (.6 + tilt)) * (r-element*.6),
+						r: element * .6,
+						fill: renderOptions.get().backgroundcolor
+					});
+				}
+			},
+			"dotcircle": {
+				contains: ["d", "k", "q", "w","th"],
+				draw: function (ctx, x, y, r,element, tilt) {
+					ctx.drawShape('circle', 0, {
+						cx: x + Math.cos(Math.PI * (.6 + tilt)) * (r-element*.6),
+						cy: y + Math.sin(Math.PI * (.6 + tilt)) * (r-element*.6),
+						r: element * .1
+					});
+					ctx.drawShape('circle', 1, {
+						cx: x + Math.cos(Math.PI * (1.6 + tilt)) * (r-element*.6),
+						cy: y + Math.sin(Math.PI * (1.6 + tilt)) * (r-element*.6),
+						r: element * .6,
+						fill: renderOptions.get().backgroundcolor
+					});
+				}
+			},
+			"twodots": {
+				contains: ["f", "l", "r", "x","ng"],
+				draw: function (ctx, x, y, r,element, tilt) {
+					ctx.drawShape('circle', 0, {
+						cx: x + Math.cos(Math.PI * (1.6 + tilt)) * (r-element*.6),
+						cy: y + Math.sin(Math.PI * (1.6 + tilt)) * (r-element*.6),
+						r: element * .1
+					});
+					ctx.drawShape('circle', 0, {
+						cx: x + Math.cos(Math.PI * (.6 + tilt)) * (r-element*.6),
+						cy: y + Math.sin(Math.PI * (.6 + tilt)) * (r-element*.6),
+						r: element * .1
+					});
+				}
+			},
+			"dot": {
+				contains: ["g", "m", "s", "y"],
+				draw: function (ctx, x, y, r, element,tilt) {
+					ctx.drawShape('circle', 0, {
+						cx: x + Math.cos(Math.PI * (1.5 + tilt)) * (r-element*.6),
+						cy: y + Math.sin(Math.PI * (1.5 + tilt)) * (r-element*.6),
+						r: element * .1
+					});
+				}
+			}
+		}
+	}
+	getDeco(char) { // return name of decorator the given character is assigned to
+		let rtrn = false;
+		Object.keys(this.tkgtable).forEach(row => {
+			if (includes(this.tkgtable[row].contains, char)) rtrn = row;
+		});
+		return rtrn != "null" ? rtrn : false;
+	}
+}
+
+export class tkgVowel {
+	constructor() {
+		this.tkgtable = {
+			/* example: { // name of group
+				contains: [...], // array of characters for which the handling and properties apply
+				draw: function (ctx, x,y,r,tilt){ // position of items to be placed, radius and tilt of graphics
+					SVGRenderingContext-draw instructions...
+				}
+			}*/
+			a: {
+				draw: function (ctx, x, y, r1,r2, tilt) {
+					ctx.drawShape('line', 1, {
+						x1: x + Math.cos(Math.PI * (.75 + tilt)) * r1 ,
+						y1: y + Math.sin(Math.PI * (.75 + tilt)) * r1 ,
+						x2: x ,
+						y2: y
+					});
+					ctx.drawShape('circle', 1, {
+						cx: x,
+						cy: y,
+						r: r2,
+						fill: renderOptions.get().backgroundcolor
+					});
+				}
+			},
+			e: {
+				draw: function (ctx, x, y, r1,r2, tilt) {
+					ctx.drawShape('line', 1, {
+						x1: x + Math.cos(Math.PI * (.75 + tilt)) * r1 ,
+						y1: y + Math.sin(Math.PI * (.75 + tilt)) * r1 ,
+						x2: x + Math.cos(Math.PI * (1.75 + tilt)) * r1 ,
+						y2: y + Math.sin(Math.PI * (1.75 + tilt)) * r1
+					});
+					ctx.drawShape('circle', 1, {
+						cx: x,
+						cy: y,
+						r: r2,
+						fill: renderOptions.get().backgroundcolor
+					});
+				}
+			},
+			i: {
+				draw: function (ctx, x, y, r1,r2, tilt) {
+					ctx.drawShape('line', 1, {
+						x1: x + Math.cos(Math.PI * (.75 + tilt)) * r1 ,
+						y1: y + Math.sin(Math.PI * (.75 + tilt)) * r1 ,
+						x2: x + Math.cos(Math.PI * (1.75 + tilt)) * r1 ,
+						y2: y + Math.sin(Math.PI * (1.75 + tilt)) * r1
+					});
+					ctx.drawShape('path', 1, {
+						d: ctx.circularArc(x, y, r1-(r1-r2) * .5, Math.PI * (1.75 + tilt), Math.PI * (2.75 + tilt),"minor")
+					});
+					ctx.drawShape('circle', 1, {
+						cx: x,
+						cy: y,
+						r: r2,
+						fill: renderOptions.get().backgroundcolor
+					});
+				}
+			},
+			o: {
+				draw: function (ctx, x, y, r1,r2, tilt) {
+					for (let i=1;i<=3;i++){
+						ctx.drawShape('line', 1, {
+						x1: x + Math.cos(Math.PI * (1.5+2/3*i + tilt)) * r1 ,
+						y1: y + Math.sin(Math.PI * (1.5+2/3*i + tilt)) * r1 ,
+						x2: x,
+						y2: y
+					});
+			}
+					ctx.drawShape('circle', 1, {
+						cx: x,
+						cy: y,
+						r: r2,
+						fill: renderOptions.get().backgroundcolor
+					});
+				}
+			},
+			u: {
+				draw: function (ctx, x, y, r1,r2, tilt) {
+					for (let i=1;i<=3;i++){
+						ctx.drawShape('line', 1, {
+						x1: x + Math.cos(Math.PI * (1.5+2/3*i + tilt)) * r1 ,
+						y1: y + Math.sin(Math.PI * (1.5+2/3*i + tilt)) * r1 ,
+						x2: x,
+						y2: y
+					});
+			}
+					ctx.drawShape('path', 1, {
+						d: ctx.circularArc(x, y, r1-(r1-r2) * .5, Math.PI * (1.5+2/3 + tilt), Math.PI * (1.5+2/3*2 + tilt),"minor")
+					});
+					ctx.drawShape('circle', 1, {
+						cx: x,
+						cy: y,
+						r: r2,
+						fill: renderOptions.get().backgroundcolor
+					});
+				}
+			}
+		}
+	}
+	getVowel(char) { // return name of decorator the given character is assigned to
+		let rtrn = false;
+		if (char in this.tkgtable) rtrn = char;
+		return rtrn != "null" ? rtrn : false;
+	}
+}
+
+/**Copyright 2020-2021 Mightyfrong, erroronline1, ModisR
+ * 
+ * This file is part of the Gallifreyan Translation Helper,
+ * henceforth referred to as "the GTH".
+ * 
+ * The GTH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The GTH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the GTH.  If not, see <https://www.gnu.org/licenses/>.
+ */
---
+++ js/tkg/render.js
@@ -0,0 +1,162 @@
+import {
+	includes,
+	dimensionObj as dimension
+} from '../main.js'
+import {
+	consonant
+} from './setup.js';
+import {
+	tkgBase,
+	tkgDeco,
+	tkgVowel
+} from './glyphs.js'
+
+let canvas = {}; // canvas properties
+let option; // user selected render options handler
+let glyphs = { // glyph dimensions object
+	num: 0,
+	width: 0,
+	height: 0
+};
+
+const base = new tkgBase();
+const deco = new tkgDeco();
+const vowel = new tkgVowel();
+
+export function render(input, renderOptions, unsupportedCharacters, SVGRenderingContext) {
+	option = renderOptions.get();
+	// convert input-string to grouped array and determine number of groups
+	let groupedInput = tkgGrouped.groups(input.toLowerCase());
+	glyphs.num = 0; // reset for new input
+
+	groupedInput.forEach(word => {
+		word.forEach(groups => {
+			groups.forEach(group => { // determine maximum expansion due to stacking and amount of groups
+				if (option.stack < 1 + 2 * group.length) option.stack = 1 + 2 * group.length;
+				glyphs.num++;
+			});
+		});
+		glyphs.num++;
+	})
+	// initialize widths, heights, default-values, draw-object
+	glyphs.width = consonant * option.stack;
+	glyphs.height = glyphs.width * 1.5;
+
+	// set canvas scale according to number of groups times glyphs.width
+	canvas["currentX"] = 0;
+	canvas["currentY"] = glyphs.height * .4;
+	canvas["width"] = dimension.canvas(glyphs, option.maxWidth).width;
+	canvas["height"] = dimension.canvas(glyphs, option.maxWidth).height;
+	const ctx = new SVGRenderingContext(canvas.width, canvas.height);
+
+	groupedInput.forEach(words => { // loop through sentence
+		words.forEach(groups => { // loop through words
+			groups.forEach(group => { // loop through character-groups
+				// prepare resizing for stacked characters
+				var stack = group.length;
+				// reset offsets but hand over possible resizing factor
+				tkgGrouped.resetOffset(stack, group.join(''));
+				// iterate through characters within group
+				for (var l = 0; l < group.length; l++) {
+					// adjust offset properties according to former character/base
+					if (l > 0) tkgGrouped.setOffset();
+					// draw
+					tkgDraw(ctx, group[l], tkgGrouped);
+					if (!base.getBase(group[l])) unsupportedCharacters.add(group[l]);
+
+					// text output for undefined characters as well for informational purpose
+					if (tkgGrouped.offset == 0) ctx.drawText(tkgGrouped.currentGroupText, {
+						x: canvas.currentX,
+						y: canvas.currentY + glyphs.height * .5
+					});
+				}
+			});
+		});
+		tkgGrouped.resetOffset();
+		tkgDraw(ctx, " ", tkgGrouped);
+	});
+
+	// complain about undefined characters
+	unsupportedCharacters.get();
+
+	return ctx;
+}
+
+// set rules for grouping
+let tkgGrouped = {
+	groups: function (input) {
+		// creates a multidimensional array for
+		// sentence -> words -> groups -> single letters
+		let sentence = [];
+		let splitinput = input.trim().replace(/\s+/g, " ").split(" "); // trim and strip multiple whitespaces, split input to single words and iterate through these
+		splitinput.forEach(sword => {
+			sentence.push([]); // init new word
+			let group = [];
+			for (var i = 0; i < sword.length; i++) { // iterate through word
+				var current = sword[i],
+					currenttwo = sword[i] + sword[i + 1];
+				// add double latin characters to group
+				if (includes(["th", "ng"], currenttwo)) {
+					current = currenttwo;
+					i++;
+				}
+				if (group.length > 0 && option.stacking) {
+					// add to former group if not full
+					if (includes("aeiou", group[group.length - 1][group[group.length - 1].length - 1]) && includes("aeoiu", current)) group[group.length - 1].push("א")
+					group[group.length - 1].push(current)
+				} else { // create current group
+					if (includes("aeoiu", current)) group.push(["א", current]);
+					else group.push([current]);
+				}
+			}
+			sentence[sentence.length - 1].push(group); // append group to last word
+		});
+		return sentence;
+	},
+	resetOffset: function (stack, currentGroupText = '') {
+		this.carriagereturn = false; // true overrides setting the pointer-position to the next character
+		this.resize = 1 + 1 * stack; // consonant-resize-factor, something the power of null is one
+		this.offset = 0; // counter of stacked objects, used for positioning the translated letters on top of the drawings
+		this.currentGroupText = currentGroupText;
+	},
+	setOffset: function () {
+		this.offset++;
+		this.carriagereturn = true;
+		this.resize -= 1;
+	}
+}
+
+// draw instructions for base + decoration
+function tkgDraw(ctx, letter, grouped) {
+	if (!grouped.carriagereturn) { // if not grouped set pointer to next letter position or initiate next line if canvas boundary is reached
+		// position pointer
+		canvas = dimension.carriageReturn(canvas, glyphs, 1);
+	}
+	//define tilt based on stack-number to make the glyphs less monotonous
+	let tilt = .25 - .0625 * (grouped.offset + 1);
+	// draw base
+	if (base.getBase(letter)) base.tkgtable[base.getBase(letter)].draw(ctx, canvas.currentX, canvas.currentY, consonant * grouped.resize, tilt);
+	// draw decorators
+	if (deco.getDeco(letter)) deco.tkgtable[deco.getDeco(letter)].draw(ctx, canvas.currentX, canvas.currentY, consonant * grouped.resize, consonant,tilt);
+	// draw vowels
+	if (vowel.getVowel(letter)) vowel.tkgtable[vowel.getVowel(letter)].draw(ctx, canvas.currentX, canvas.currentY, consonant * (grouped.resize+1),consonant * (grouped.resize-1),tilt);
+}
+
+/**Copyright 2020-2021 Mightyfrong, erroronline1, ModisR
+ *
+ * This file is part of the Gallifreyan Translation Helper,
+ * henceforth referred to as "the GTH".
+ *
+ * The GTH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The GTH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the GTH.  If not, see <https://www.gnu.org/licenses/>.
+ */
