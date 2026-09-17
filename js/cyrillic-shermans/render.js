2d0ed267b33e471af38b5abcc42667194fec4d0f
levenorts <levenorts@gmail.com>
2026-09-17 13:44:23 +0300

    Update render.js

--- js/cyrillic-shermans/render.js
+++ js/cyrillic-shermans/render.js
@@ -156,10 +156,12 @@ let grouped = {
 					let former = group[group.length - 1][group[group.length - 1].length - 1];
 					let currentBase = base.getBase(current);
 					let formerBase = base.getBase(former);
-					console.log('current:', current, 'base:', currentBase);
-    				console.log('former:', former, 'base:', formerBase);
-   					console.log('option.combine:', option.combine);
-    				console.log('option.stacking:', option.stacking);
+ 					   console.log('---');
+						console.log('current:', current, 'base:', currentBase);
+					    console.log('former:', former, 'base:', formerBase);
+					    console.log('option.combine:', option.combine);
+					    console.log('option.stacking:', option.stacking);
+					    console.log('currentBase === formerBase:', currentBase === formerBase);
 
 					// 1. Гласная к согласной (stacking)
 					let vowelToConsonant = option.stacking &&
